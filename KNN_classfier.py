ce39d5b72c8f300cbee8308683c5a725c0a234cd
Kaio1005 <desakaio4@gmail.com>
2023-11-29 16:49:22 -0300

    Ended KNN implementation

--- KNN_classfier.py
+++ KNN_classfier.py
@@ -3,14 +3,16 @@ from collections import Counter
 
 class KNN_classfier:
 
-    def __init__ (self, _k, _train, _train_classes) -> None:
+    def __init__ (self, _k, _train, _test, _train_classes, _test_classes) -> None:
         self.k = _k
         self.train = _train
+        self.test = _test
         self.train_classes = _train_classes
+        self.test_classes = _test_classes
 
     def select_neighbors (self, neighbor_dict):
         selected = []
-        keys = neighbor_dict.keys()
+        keys = list(neighbor_dict.keys())
         
         for i in range (self.k):
             selected.append(keys[i])
@@ -20,8 +22,8 @@ class KNN_classfier:
 
     def classifie (self, _target):
         distances = {}
-        for i, player in enumerate(self.train):
-            distances[i] = np.distance.euclidean(_target, player)
+        for i, player in enumerate(self.train): 
+            distances[i] = self.euclidean_dist(_target, player)
         
         sorted_neighbors = sorted(distances.items(), key=lambda x:x[1])
         sorted_neighbors = dict(sorted_neighbors)
@@ -30,11 +32,65 @@ class KNN_classfier:
 
         classes = []
         for neighbor in neighbors:
-            classes.append(self.train_classes[neighbor])
+            classes.append(self.train_classes[neighbor][0])
 
         counter = Counter(classes)
         player_class = counter.most_common(n = 1)[0][0]
 
         return (player_class)
     
+    def euclidean_dist (self, u, v):
+        return np.sqrt(np.sum((u-v)**2))
     
+    def test_fun (self):
+        classes = {}
+        for i, player in enumerate(self.test):
+            classes[i] = self.classifie(player)
+        
+        TP, FP, TN, FN = self.confusion_matrix(classes)
+        accuracy = self.accuracy (TP, FP, TN, FN)
+        print(f"Accuracy is: {accuracy}")
+        precision = self.precision (TP, FP)
+        print(f"Precision is: {precision}")
+        recall = self.recall (TP, FN)
+        print(f"Recall is: {recall}")
+        F1 = self.F1 (precision, recall)
+        print(f"F1 is: {F1}")
+
+    def confusion_matrix (self, classes):
+        TP = 0
+        FP = 0
+        TN = 0
+        FN = 0
+        for i in range (len(list(classes.keys()))):
+            if (classes[i] == 1 and self.test_classes[i] == 1):
+                TP += 1
+            elif (classes[i] == 1 and self.test_classes[i] == 0):
+                FP += 1
+            elif (classes[i] == 0 and self.test_classes[i] == 0):
+                TN += 1
+            elif (classes[i] == 0 and self.test_classes[i] == 1):
+                FN += 1
+        
+        print(f"TP = {TP}")
+        print(f"FP = {FP}")
+        print(f"TN = {TN}")
+        print(f"FN = {FN}")
+
+        return (TP,FP,TN,FN)
+
+    def accuracy (self, TP, FP, TN, FN):
+        accuracy = (TP+TN) / (TP+FP+TN+FN)
+        return accuracy
+    
+    def precision (self, TP, FP):
+        precision = TP / (TP+FP)
+        return precision
+    
+    def recall (self, TP, FN):
+        recall = TP / (TP+FN)
+        return recall
+    
+    def F1 (self, precision, recall):
+        F1 = (2 * precision * recall) / (precision + recall)
+        return F1
--- main.py
+++ main.py
@@ -2,17 +2,33 @@ import sys
 import pandas as pd
 import numpy as np
 import KNN_classfier
-#ao executar colocar na linha de comando da seguinte maneira 'python3 main.py algoritmo treino teste' 
-#Se o algoritmo for KNN colocar também o valor de K após o teste 'python3 main.py algoritmo treino teste K' 
+#ao executar colocar na linha de comando da seguinte maneira 'python3 main.py algoritmo  K treino teste'  
 
 algorithm = sys.argv[1]
-train = sys.argv[2]
-test = sys.argv[3]
+K = int(sys.argv[2])
+train = sys.argv[3]
+test = sys.argv[4]
+
 
 train = pd.read_csv(train)
 test = pd.read_csv(test)
+train_classes = pd.DataFrame()
+train_classes['TARGET_5Yrs'] = train['TARGET_5Yrs'].copy(deep = True)
+train_classes = train_classes.to_numpy()
 
+train = train.drop('TARGET_5Yrs', axis = 1)
+train = train.to_numpy()
+
+test_classes = pd.DataFrame()
+test_classes['TARGET_5Yrs'] = test['TARGET_5Yrs'].copy(deep = True)
+test_classes = test_classes.to_numpy()
+
+test = test.drop('TARGET_5Yrs', axis = 1)
+test = test.to_numpy()
 
 if algorithm == 'KNN':
-    K = int(sys.argv[4])
-    classfier = KNN_classfier(K, train, test)
+    classfier = KNN_classfier.KNN_classfier(K, train, test, train_classes, test_classes)
+    classfier.test_fun()
+elif algorithm == 'K-means':
+    pass
+    
